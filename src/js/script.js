0c73e7728dcf4d927bf4de658f4b41f2f4f9adb0
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-30 10:50:26 -0300

    localStorage pronto

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 const inputTaskAdd = document.querySelector("[data-input-add]");
 const btnAdd = document.querySelector("[data-btn-add]");
 const btnDelete = document.querySelectorAll("[data-btn-delete]");
-const btnEdit = document.querySelectorAll("[data-btn-edit]");
 const taskListContainer = document.querySelector(".task-list-container");
-
 const validateInput = () => inputTaskAdd.value.trim().length > 0;
+
+// carrega as tasks que estão no localStorage
+refreshTasksUsingLocalStorage();
 
 function removeInputErrorClass() {
   const inputIsValid = validateInput();
@@ -14,13 +15,16 @@ function removeInputErrorClass() {
   }
 }
 
-// adiciona tarefa
-function handleAdd() {
+/* Funções das tasks (adicionar, deletar, completar)
+ *****************************************************/
+
+// adiciona task
+function handleAddTask() {
   const inputIsValid = validateInput();
 
-  // verifica se o input está vazio
+  // verifica o input
   if (!inputIsValid) {
-    alert("Certfique-se de preencher o campo!");
+    alert("Preencha o campo antes de adicionar alguma tarefa!");
     inputTaskAdd.classList.add("error");
     return;
   }
@@ -32,21 +36,8 @@ function handleAdd() {
 
   // cria o texto da task
   const taskContent = document.createElement("p");
-  taskContent.innerHTML = inputTaskAdd.value;
+  taskContent.innerText = inputTaskAdd.value;
   taskItem.appendChild(taskContent);
-
-  // listener de tarefa completa
-  taskItem.addEventListener("click", () => {
-    handleComplete(taskContent);
-  });
-
-  // listeners de drag
-  taskItem.addEventListener("dragstart", ({ target }) => {
-    dragstart(target);
-  });
-  taskItem.addEventListener("dragend", ({ target }) => {
-    dragend(target);
-  });
 
   // cria o button da task
   const btnDelete = document.createElement("button");
@@ -57,19 +48,35 @@ function handleAdd() {
   btnDelete.appendChild(iconDelete);
   taskItem.appendChild(btnDelete);
 
-  btnDelete.addEventListener("click", () => {
-    handleDelete(taskItem, taskContent);
-  });
-
   // adiciona task a lista de tasks
   taskListContainer.appendChild(taskItem);
 
+  // listener de delete
+  btnDelete.addEventListener("click", () => {
+    handleDeleteTask(taskItem, taskContent);
+  });
+
+  // listener de task completa
+  taskItem.addEventListener("click", () => {
+    handleCompleteTask(taskContent);
+  });
+
+  // listeners de drag
+  taskItem.addEventListener("dragstart", ({ target }) => {
+    dragstart(target);
+  });
+  taskItem.addEventListener("dragend", ({ target }) => {
+    dragend(target);
+  });
+
   // limpa input após adicionar a task
   inputTaskAdd.value = "";
+
+  updateLocalStorage();
 }
 
-// deletar tarefa
-function handleDelete(taskItem, taskContent) {
+// deletar task
+function handleDeleteTask(taskItem, taskContent) {
   const tasks = taskListContainer.childNodes;
 
   for (const task of tasks) {
@@ -77,10 +84,12 @@ function handleDelete(taskItem, taskContent) {
       taskItem.remove();
     }
   }
+
+  updateLocalStorage();
 }
 
-// completar tarefa
-function handleComplete(taskContent) {
+// completar task
+function handleCompleteTask(taskContent) {
   const tasks = taskListContainer.childNodes;
 
   for (const task of tasks) {
@@ -88,16 +97,79 @@ function handleComplete(taskContent) {
       task.firstChild.classList.toggle("completed");
     }
   }
+
+  updateLocalStorage();
 }
 
-// listeners para adicionar tarefa
-inputTaskAdd.addEventListener("keydown", (event) => {
-  if (event.keyCode === 13) handleAdd();
-});
-btnAdd.addEventListener("click", handleAdd);
+/* Funções de localStorage
+ **********************************************/
+function updateLocalStorage() {
+  const tasks = taskListContainer.childNodes;
 
-/* funções de drag and drop TESTE */
+  const localStorageTasks = [...tasks].map((task) => {
+    const content = task.firstChild;
+    const isCompleted = content.classList.contains("completed");
 
+    return {
+      description: content.innerHTML,
+      isCompleted,
+    };
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+}
+
+function refreshTasksUsingLocalStorage() {
+  const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+
+  for (const task of tasksFromLocalStorage) {
+    // cria o container da task
+    const taskItem = document.createElement("div");
+    taskItem.setAttribute("draggable", true);
+    taskItem.classList = "task-item";
+
+    // cria o texto da task
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+    if (task.isCompleted) {
+      taskContent.classList.add("completed");
+    }
+    taskItem.appendChild(taskContent);
+
+    // cria o button da task
+    const btnDelete = document.createElement("button");
+    btnDelete.setAttribute("data-btn-delete", "");
+
+    const iconDelete = document.createElement("i");
+    iconDelete.classList = "fa-regular fa-trash-can";
+    btnDelete.appendChild(iconDelete);
+    taskItem.appendChild(btnDelete);
+
+    // adiciona task a lista de tasks
+    taskListContainer.appendChild(taskItem);
+
+    // listener de delete
+    btnDelete.addEventListener("click", () => {
+      handleDeleteTask(taskItem, taskContent);
+    });
+
+    // listener de tarefa completa
+    taskItem.addEventListener("click", () => {
+      handleCompleteTask(taskContent);
+    });
+
+    // listeners de drag
+    taskItem.addEventListener("dragstart", ({ target }) => {
+      dragstart(target);
+    });
+    taskItem.addEventListener("dragend", ({ target }) => {
+      dragend(target);
+    });
+  }
+}
+
+/* Funções de drag and drop
+ *************************************************/
 // funções de drag
 function dragstart(target) {
   target.classList.add("is-dragging");
@@ -116,7 +188,13 @@ function dragleave(target) {
   target.classList.remove("drag-over");
 }
 
-/* event listeners */
+/* Event listeners
+ *******************************************************/
+inputTaskAdd.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) handleAddTask();
+});
+btnAdd.addEventListener("click", handleAddTask);
+
 inputTaskAdd.addEventListener("input", removeInputErrorClass);
 
 taskListContainer.addEventListener("dragover", ({ target }) => {
